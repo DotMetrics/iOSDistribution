b9fad0fca4ec840428460edcd32f80db555ea0b8
Denis Jakuš <denis@inquis-it.hr>
2021-11-17 12:28:59 +0100

    reversing version number

--- Dotmetrics.framework/Headers/DotmetricsSession.h
+++ Dotmetrics.framework/Headers/DotmetricsSession.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 #import <CoreLocation/CoreLocation.h>
 
-#define CLIENT_VERSION              @"1.6.6"      // The version of this library
+#define CLIENT_VERSION              @"1.6.5"      // The version of this library
 
 /*!
  @class DotmetricsSession
