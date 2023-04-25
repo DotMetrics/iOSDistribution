d845f08394ffc84db81b4542ef1ad90bf1b69926
Denis Jakuš <denis@inquis-it.hr>
2023-04-25 11:49:13 +0200

    version 1.7.5

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
                     name: "Dotmetrics",
-                    url: "https://github.com/DotMetrics/iOSDistribution/releases/download/1.7.4/Dotmetrics.xcframework.zip",
-                    checksum: "2817a50ba1fc1e726de52773c642fdcb30eef4c746edff7fcd1ebe30542371c1"
+                    url: "https://github.com/DotMetrics/iOSDistribution/releases/download/1.7.5/Dotmetrics.xcframework.zip",
+                    checksum: "e1b150b1d29e85976d55f1238cee258dc2b5909b8a95cd8204273b739a2f4f1e"
                 )
     ]
 )
